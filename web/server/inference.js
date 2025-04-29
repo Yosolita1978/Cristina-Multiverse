ab8877e35769c132b03cf90282c399b4d9ff73e2
Cristina Rodriguez <yosola@gmail.com>
2025-04-29 09:44:36 -0700

    Changing the API call

--- web/server/inference.js
+++ web/server/inference.js
@@ -1,27 +1,21 @@
-const endpointId = process.env.GOOGLE_CLOUD_ENDPOINT_ID;
-const project = process.env.GOOGLE_PROJECT;
-const location = process.env.GOOGLE_LOCATION;
-
-const {PredictionServiceClient} = require('@google-cloud/aiplatform');
-
-// Specifies the location of the api endpoint
-const clientOptions = {
-    apiEndpoint: 'us-central1-aiplatform.googleapis.com',
-};
-
-const predictionServiceClient = new PredictionServiceClient(clientOptions);
+const HUGGINGFACE_API_URL = "https://xnoufaxvh12gt3v6.us-east4.gcp.endpoints.huggingface.cloud"; // Set this in your env file
 
 async function runModel(prompt) {
-  const request = {
-    endpoint: `projects/${project}/locations/${location}/endpoints/${endpointId}`,
-    instances: [
-        { stringValue: prompt}
-    ],
-  };
+  const response = await fetch(HUGGINGFACE_API_URL, {
+    headers: { 
+      "Accept": "image/png",
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({
+      inputs: prompt,
+      parameters: {}
+    })
+  });
 
-  const [response] = await predictionServiceClient.predict(request);
+  const result = await response.content;
   
-  return response.predictions[0].stringValue;
+  return result; 
 }
 
 exports.runModel = runModel;
